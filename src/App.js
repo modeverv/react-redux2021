18f60e53efca2ce291af9f50c9c0f167d2b32edb
Seijiro Ikehata <modeverv@gmail.com>
2021-02-07 00:38:07 +0900

    20210207

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-
 import React, { Component } from 'react';
-
+/*
 class App extends Component {
   render() {
-    //const greeting = "Hi Tom";
-    //const jsx = <h1>{greeting}</h1>
-    //return <div className="hoge" onClick={() => { alert(1) }}>{jsx}</div>
-    //return <div className="hoge" onClick={() => { alert(1) }}>{jsx}</div>
     return <React.Fragment>
       <label htmlFor="bar">アアアアア</label>
       <input type="text" id="bar" onChange={() => {console.log("changed")}}/>
     </React.Fragment>
   }
 }
+*/
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <Cat />
+        <Cat />
+        <Cat />
+        <Cat />
+      </ul>
+    </div>
+  );
+};
+const Cat = () => {
+  return <li>mew!!</li>;
+};
 
 export default App;
